9ea2bca48d649402aa637f63d3f315a685824ad1
Michal Stoma <m.stoma@gmail.com>
2019-07-06 22:51:13 +0200

    Acronym

---
+++ acronym/src/main/groovy/Acronym.groovy
@@ -0,0 +1,11 @@
+class Acronym {
+
+    static String abbreviate(String phrase) {
+	    phrase.replace('_', '')
+	    	.split(/(?!')\W/)
+	    	.findAll {!it.isEmpty()}
+	    	.collect {it[0].toUpperCase()}
+	    	.join()
+    }
+
+}
