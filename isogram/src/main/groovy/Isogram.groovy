c1fb871d8c97f8557205f79d66592a60c85e2862
Michal Stoma <m.stoma@gmail.com>
2019-07-06 23:36:50 +0200

    Isogram: different regex expression

--- isogram/src/main/groovy/Isogram.groovy
+++ isogram/src/main/groovy/Isogram.groovy
@@ -1,6 +1,6 @@
 class Isogram {
     static boolean isIsogram(String phrase) {
-        def chars = phrase.toLowerCase().findAll('\\p{Alnum}')
+        def chars = phrase.toLowerCase().findAll(/[a-zA-Z0-9]/)
         chars.size() == chars.toSet().size()
     }
 }
