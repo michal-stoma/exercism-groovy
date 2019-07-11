f50989c88593d76c7d36a31e6ed802cddb264218
Michal Stoma <m.stoma@gmail.com>
2019-07-11 21:06:46 +0200

    Gigasecond: second iteration.

--- gigasecond/src/main/groovy/Gigasecond.groovy
+++ gigasecond/src/main/groovy/Gigasecond.groovy
@@ -2,14 +2,14 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 class Gigasecond {
-	static long GIGASECOND = 10**9
+	static final int GIGASECOND = 10**9
 
     static LocalDateTime add(LocalDate date) {
-    	date.atStartOfDay().plusSeconds(GIGASECOND)
+    	add(date.atStartOfDay())
     }
 
     static LocalDateTime add(LocalDateTime date) {
-    	date.plusSeconds(GIGASECOND)
+    	date + GIGASECOND
     }
 
 }
