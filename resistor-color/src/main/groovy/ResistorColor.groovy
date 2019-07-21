0ac813c0cbc97d039ea8e05b7d4f080a50839726
Michal Stoma <michal.stoma@tivo.com>
2019-07-21 11:23:17 +0200

    Resistor Color

---
+++ resistor-color/src/main/groovy/ResistorColor.groovy
@@ -0,0 +1,18 @@
+class ResistorColor {
+
+	static final colors = ['black',
+                           'brown',
+                           'red',
+                           'orange',
+	                       'yellow',
+	                       'green',
+	                       'blue',
+	                       'violet',
+	                       'grey',
+	                       'white']
+
+    static colorCode(color) {
+        colors.indexOf(color)
+    }
+
+}
